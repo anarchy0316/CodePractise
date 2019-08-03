8686079c59d09fed37aa15592f10334f2ee8cc25
Akira <anarchy0316@gmail.com>
2019-08-03 18:09:01 +0800

    20 completed

---
+++ LeetCode/LeetCode/0020_Valid_Parentheses.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.LeetCode
+{
+    public class _0020_Valid_Parentheses
+    {
+        public bool IsValid(string s)
+        {
+            Dictionary<char, char> dic = new Dictionary<char, char>
+            {
+                { ')','('},
+                { ']','['},
+                { '}','{'},
+            };
+            Stack<char> stk = new Stack<char>();
+            foreach (var c in s)
+            {
+                if (stk.Count > 0 && (dic.ContainsKey(c) && stk.Peek() == dic[c]))
+                {
+                    stk.Pop();
+                }
+                else
+                {
+                    stk.Push(c);
+                }
+            }
+            return stk.Count == 0;
+        }
+        /// <summary>
+        /// ASCII Hack
+        /// </summary>
+        /// <param name="s"></param>
+        /// <returns></returns>
+        public bool IsValid2(string s)
+        {
+
+            Stack<char> stk = new Stack<char>();
+            foreach (var c in s)
+            {
+                if (stk.Count > 0 && (c - stk.Peek() == 1 || c - stk.Peek() == 2))
+                {
+                    stk.Pop();
+                }
+                else
+                {
+                    stk.Push(c);
+                }
+            }
+            return stk.Count == 0;
+        }
+    }
+}
--- LeetCode/LeetCode/Program.cs
+++ LeetCode/LeetCode/Program.cs
@@ -10,13 +10,8 @@ namespace LeetCode.LeetCode
     {
         static void Main(string[] args)
         {
-            _0019_Remove_Nth_Node_From_End_of_List a = new _0019_Remove_Nth_Node_From_End_of_List();
-            var head = new _0019_Remove_Nth_Node_From_End_of_List.ListNode(1);
-            head.next = new _0019_Remove_Nth_Node_From_End_of_List.ListNode(2);
-            head.next.next = new _0019_Remove_Nth_Node_From_End_of_List.ListNode(3);
-            head.next.next.next = new _0019_Remove_Nth_Node_From_End_of_List.ListNode(4);
-            head.next.next.next.next = new _0019_Remove_Nth_Node_From_End_of_List.ListNode(5);
-            a.RemoveNthFromEnd(head, 2);
+            _0020_Valid_Parentheses a = new _0020_Valid_Parentheses();
+            Console.WriteLine(a.IsValid("()"));
         }
     }
 }
