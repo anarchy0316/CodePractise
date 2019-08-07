7974f24c2cb031053d6fb7f9e2b2a9bccae4b3b3
Akira <anarchy0316@gmail.com>
2019-08-07 16:06:40 +0800

    28 cp

---
+++ LeetCode/LeetCode/0028_Implement_Strstr.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.LeetCode
+{
+    public class _0028_Implement_Strstr
+    {
+        public int StrStr(string haystack, string needle)
+        {
+            if (needle == string.Empty) return 0;
+            for (int i = 0; i <= haystack.Length - needle.Length; i++)
+            {
+                for (int j = 0; j < needle.Length; j++)
+                {
+                    if (needle[j] != haystack[i + j])
+                    {
+                        break;
+                    }
+                    else
+                    {
+                        if (j == needle.Length - 1)
+                        {
+                            return i;
+                        }
+                    }
+                }
+            }
+            return -1;
+        }
+    }
+}
