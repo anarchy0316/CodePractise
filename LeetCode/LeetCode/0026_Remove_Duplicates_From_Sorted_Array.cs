7f309be708dc74323f112441c3ac4dab563fe1e7
Akira <anarchy0316@gmail.com>
2019-08-07 15:25:01 +0800

    26 cp

---
+++ LeetCode/LeetCode/0026_Remove_Duplicates_From_Sorted_Array.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.LeetCode
+{
+    //https://leetcode-cn.com/problems/remove-duplicates-from-sorted-array/
+    public class _0026_Remove_Duplicates_From_Sorted_Array
+    {
+        public int RemoveDuplicates(int[] nums)
+        {
+            if (nums.Length == 0)
+            {
+                return 0;
+            }
+            int CurrentPos = 0;
+            for (int i = 1; i < nums.Length; i++)
+            {
+                if (nums[CurrentPos] != nums[i])
+                {
+                    nums[CurrentPos + 1] = nums[i];
+                    CurrentPos++;
+                }
+            }
+            return CurrentPos + 1;
+        }
+    }
+}
